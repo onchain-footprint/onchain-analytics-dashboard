58628d4a0411df8f0d4cfbb8a7a5cea453804803
Vishal Bangar <vishalbangaar@gmail.com>
2025-11-12 11:19:53 +0530

    fix: ignore react-native async-storage warning for web build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // Ignore React Native async-storage import for web builds
+    config.resolve.alias['@react-native-async-storage/async-storage'] = false;
+    return config;
+  },
 };
+
 module.exports = nextConfig;
